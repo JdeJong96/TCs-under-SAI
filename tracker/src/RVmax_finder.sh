259ec22dbd380ee203f57a97038136e7f101066f
Jasper de Jong <j.dejong3@uu.nl>
2025-05-21 15:22:06 +0200

    update jobscript

--- tracker/src/RVmax_finder.sh
+++ tracker/src/RVmax_finder.sh
@@ -2,12 +2,11 @@
 #SBATCH -c 192 # adjust depending on # of unfinished tasks (dry run RV_max_finder)
 #SBATCH -p genoa
 #SBATCH -t 2:00:00  # processing 1 year of data takes 52min on 64 cores
-#SBATCH -o RVmax_finder.seeds.log.ref-%j
+#SBATCH -o RVmax_finder.log.sai-%j
 
 source ~/.bashrc
 conda activate geo
 export OMP_NUM_THREADS=1 # prevent subprocesses from using paralellism
-cd /home/jasperdj/files_rene
 echo "experiment: $1"
 echo "run ensemble: $2"
 python -u RVmax_finder.py $1 $2
