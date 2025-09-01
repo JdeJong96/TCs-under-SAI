b3d44383fb5a60e38556cca3850e7261933547c6
Jasper de Jong <j.dejong3@uu.nl>
2025-09-01 16:06:29 +0200

    edit for new job

--- tracker/src/Tracking_TC_RV.sh
+++ tracker/src/Tracking_TC_RV.sh
@@ -8,11 +8,11 @@ source ~/.bashrc
 conda activate geo
 export OMP_NUM_THREADS=1 # prevent subprocesses from using paralellism
 script=Tracking_TC_RV
-#python -u $script.py ref 1 > $script.log.ref.1 &
-#python -u $script.py ref 2 > $script.log.ref.2 &
-#python -u $script.py ref 3 > $script.log.ref.3 &
-#python -u $script.py ref 4 > $script.log.ref.4 &
-#python -u $script.py ref 5 > $script.log.ref.5 &
+python -u $script.py ref 1 > $script.log.ref.1 &
+python -u $script.py ref 2 > $script.log.ref.2 &
+python -u $script.py ref 3 > $script.log.ref.3 &
+python -u $script.py ref 4 > $script.log.ref.4 &
+python -u $script.py ref 5 > $script.log.ref.5 &
 python -u $script.py ref 6 > $script.log.ref.6 &
 
 #python -u $script.py rcp 1 > $script.log.rcp.1 &
@@ -22,10 +22,10 @@ python -u $script.py ref 6 > $script.log.ref.6 &
 #python -u $script.py rcp 5 > $script.log.rcp.5 &
 python -u $script.py rcp 6 > $script.log.rcp.6 &
 
-#python -u $script.py sai 1 > $script.log.sai.1 &
-#python -u $script.py sai 2 > $script.log.sai.2 &
-#python -u $script.py sai 3 > $script.log.sai.3 &
-#python -u $script.py sai 4 > $script.log.sai.4 &
-#python -u $script.py sai 5 > $script.log.sai.5 &
+python -u $script.py sai 1 > $script.log.sai.1 &
+python -u $script.py sai 2 > $script.log.sai.2 &
+python -u $script.py sai 3 > $script.log.sai.3 &
+python -u $script.py sai 4 > $script.log.sai.4 &
+python -u $script.py sai 5 > $script.log.sai.5 &
 python -u $script.py sai 6 > $script.log.sai.6 &
 wait
